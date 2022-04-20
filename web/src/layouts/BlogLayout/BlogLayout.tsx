4e21ea644eb8ac24df20ca1cbb952c562de5701d
libregd <libregd@outlook.com>
2022-04-21 02:23:16 +0800

    chapter4 completed

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,28 +1,42 @@
 type BlogLayoutProps = {
   children?: React.ReactNode
 }
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
   return (
     <>
       <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.contact()}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="flex-between">
+            <h1>
+              <Link to={routes.home()}>Redwood Blog</Link>
+            </h1>
+            {isAuthenticated ? (
+              <div>
+                <span>Logged in as {currentUser.email}</span>{' '}
+                <button type="button" onClick={logOut}>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <Link to={routes.login()}>Login</Link>
+            )}
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={routes.home()}>Home</Link>
+              </li>
+              <li>
+                <Link to={routes.about()}>About</Link>
+              </li>
+              <li>
+                <Link to={routes.contact()}>Contact</Link>
+              </li>
+            </ul>
+          </nav>
       </header>
       <main>{children}</main>
     </>
